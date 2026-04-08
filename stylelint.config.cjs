d543f8f891c6cbd81f881594a77aa9258b423d73
yutaka <whiteraccoon.breakable@gmail.com>
2026-04-08 17:08:34 +0000

    fix(web): allow stylelint null config refs #103

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['stylelint-config-standard'],
   ignoreFiles: ['**/.next/**', '**/coverage/**', '**/node_modules/**'],
   rules: {
+    // oxlint-disable-next-line unicorn/no-null -- stylelint uses null to disable inherited rules.
     'selector-class-pattern': null,
   },
 };
